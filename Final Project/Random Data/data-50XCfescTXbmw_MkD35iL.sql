bab8cad52dae0f1f5c1d38220ca0d606b3d37c4f
jcsoutherland <jcsoutherland@ksu.edu>
2021-11-17 17:12:22 -0600

    Upload Database Creation SQL

---
+++ Final Project/Random Data/data-50XCfescTXbmw_MkD35iL.sql
@@ -0,0 +1,128 @@
+DROP TABLE IF EXISTS `myTable`;
+
+CREATE TABLE `myTable` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `Location` varchar(255),
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Jonesboro"),
+  ("Olympia"),
+  ("Covington"),
+  ("Springdale"),
+  ("Metairie"),
+  ("Little Rock"),
+  ("San Francisco"),
+  ("Tuscaloosa"),
+  ("Laramie"),
+  ("Hillsboro");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Cincinnati"),
+  ("Pittsburgh"),
+  ("Toledo"),
+  ("Bear"),
+  ("Norman"),
+  ("Colorado Springs"),
+  ("Wilmington"),
+  ("Metairie"),
+  ("St. Petersburg"),
+  ("Essex");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Las Vegas"),
+  ("Denver"),
+  ("Frankfort"),
+  ("Jackson"),
+  ("Omaha"),
+  ("Clarksville"),
+  ("Spokane"),
+  ("Frankfort"),
+  ("Topeka"),
+  ("Stamford");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Cheyenne"),
+  ("Independence"),
+  ("Bowling Green"),
+  ("Fayetteville"),
+  ("Tulsa"),
+  ("Gary"),
+  ("San Antonio"),
+  ("Little Rock"),
+  ("Cleveland"),
+  ("Chattanooga");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Bloomington"),
+  ("Flint"),
+  ("Rock Springs"),
+  ("Overland Park"),
+  ("Joliet"),
+  ("Fort Smith"),
+  ("Evansville"),
+  ("Helena"),
+  ("Gaithersburg"),
+  ("Cleveland");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Rockville"),
+  ("Jacksonville"),
+  ("Pittsburgh"),
+  ("Denver"),
+  ("Bowling Green"),
+  ("Denver"),
+  ("Mesa"),
+  ("Bellevue"),
+  ("Cleveland"),
+  ("Houston");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Lincoln"),
+  ("Springfield"),
+  ("Warren"),
+  ("Kaneohe"),
+  ("Stamford"),
+  ("Broken Arrow"),
+  ("Huntsville"),
+  ("Allentown"),
+  ("Newark"),
+  ("South Bend");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Rockford"),
+  ("Glendale"),
+  ("Jacksonville"),
+  ("Lakewood"),
+  ("Minneapolis"),
+  ("Pocatello"),
+  ("Norman"),
+  ("Newark"),
+  ("Philadelphia"),
+  ("Lowell");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Salt Lake City"),
+  ("Toledo"),
+  ("Cambridge"),
+  ("Davenport"),
+  ("South Burlington"),
+  ("Pittsburgh"),
+  ("Frankfort"),
+  ("Bellevue"),
+  ("Mesa"),
+  ("Jackson");
+INSERT INTO `myTable` (`Location`)
+VALUES
+  ("Fort Collins"),
+  ("Tallahassee"),
+  ("Grand Rapids"),
+  ("South Bend"),
+  ("Henderson"),
+  ("Rockville"),
+  ("Vancouver"),
+  ("Spokane"),
+  ("Louisville"),
+  ("Gresham");
